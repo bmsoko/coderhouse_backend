1bc98579b146832a5a88addbe588b35063d7a7e7
Bruno Soko <bmsoko@users.noreply.github.com>
2021-09-27 22:18:51 -0300

    Entregable3

---
+++ entregable3/serverExpress.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const server = express();
+const Contenedor = require('./Contenedor');
+const miCont = new Contenedor('./productos.json');
+
+const PORT = 3000;
+
+const randomNumber = (min, max) => Math.round(Math.random() * (max - min + 1)) + min;
+
+server.get('/', (request, response, next) => {
+    response.send(`<h1>Hola Mundo ${PORT}</h1>`);
+});
+
+server.listen(PORT, () => {
+    console.log(`El server express esta corriendo en el puerto ${PORT}`);
+})
+
+server.get('/productos', async (request, response, next) => {
+    const productos = await miCont.getAll();
+    response.send({productos : productos});
+});
+
+
+server.get('/productos', async (request, response, next) => {
+    const productos = await miCont.getAll();
+    response.send(productos);
+});
+
+server.get('/productosRandom', async (request, response, next) => {
+    const productos = await miCont.getAll();
+    const id = randomNumber(1, productos.length)
+    const productoRandom = await miCont.getById(id);
+    response.send(productoRandom);
+});
+
+server.on("error", error => console.log(`Error en servidor ${error}`));
+
